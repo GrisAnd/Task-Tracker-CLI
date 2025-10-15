4896b860495bd3e47045082af7680474ed652d43
Andre Vinicius <meteor653487@gmail.com>
2025-10-15 18:31:08 -0300

    Added the command: 'list' basic functionality

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
 
     if sys.argv[1] == "add":
         add_task()
+    elif sys.argv[1] == "list":
+        list_tasks()
+    else:
+        print("Enter a valid argument")
 
 
 def add_task():
@@ -37,6 +41,18 @@ def add_task():
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
 
+    print(f"Task added successfully (ID: {task_id})")
+
+
+def list_tasks():
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    task_number = int(data['tasks'][-1]['id'])
+    i = 1
+    while i <= task_number:
+        print(data['tasks'][i])
+        i += 1
 
 if __name__ == "__main__":
     main()
